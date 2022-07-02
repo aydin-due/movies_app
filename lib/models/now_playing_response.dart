50e1efc906877bafc6e0cd2f9503cf4fbe53ac25
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-02 14:04:32 -0700

    http request

---
+++ lib/models/now_playing_response.dart
@@ -0,0 +1,159 @@
+// To parse this JSON data, do
+//
+//     final nowPlayingResponse = nowPlayingResponseFromMap(jsonString);
+
+import 'dart:convert';
+
+class NowPlayingResponse {
+  
+    NowPlayingResponse({
+        this.dates,
+        this.page,
+        this.results,
+        this.totalPages,
+        this.totalResults,
+    });
+
+    Dates dates;
+    int page;
+    List<Result> results;
+    int totalPages;
+    int totalResults;
+
+    factory NowPlayingResponse.fromJson(String str) => NowPlayingResponse.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory NowPlayingResponse.fromMap(Map<String, dynamic> json) => NowPlayingResponse(
+        dates: Dates.fromMap(json["dates"]),
+        page: json["page"],
+        results: List<Result>.from(json["results"].map((x) => Result.fromMap(x))),
+        totalPages: json["total_pages"],
+        totalResults: json["total_results"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "dates": dates.toMap(),
+        "page": page,
+        "results": List<dynamic>.from(results.map((x) => x.toMap())),
+        "total_pages": totalPages,
+        "total_results": totalResults,
+    };
+}
+
+class Dates {
+    Dates({
+        this.maximum,
+        this.minimum,
+    });
+
+    DateTime maximum;
+    DateTime minimum;
+
+    factory Dates.fromJson(String str) => Dates.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory Dates.fromMap(Map<String, dynamic> json) => Dates(
+        maximum: DateTime.parse(json["maximum"]),
+        minimum: DateTime.parse(json["minimum"]),
+    );
+
+    Map<String, dynamic> toMap() => {
+        "maximum": "${maximum.year.toString().padLeft(4, '0')}-${maximum.month.toString().padLeft(2, '0')}-${maximum.day.toString().padLeft(2, '0')}",
+        "minimum": "${minimum.year.toString().padLeft(4, '0')}-${minimum.month.toString().padLeft(2, '0')}-${minimum.day.toString().padLeft(2, '0')}",
+    };
+}
+
+class Result {
+    Result({
+        this.adult,
+        this.backdropPath,
+        this.genreIds,
+        this.id,
+        this.originalLanguage,
+        this.originalTitle,
+        this.overview,
+        this.popularity,
+        this.posterPath,
+        this.releaseDate,
+        this.title,
+        this.video,
+        this.voteAverage,
+        this.voteCount,
+    });
+
+    bool adult;
+    String backdropPath;
+    List<int> genreIds;
+    int id;
+    OriginalLanguage originalLanguage;
+    String originalTitle;
+    String overview;
+    double popularity;
+    String posterPath;
+    DateTime releaseDate;
+    String title;
+    bool video;
+    double voteAverage;
+    int voteCount;
+
+    factory Result.fromJson(String str) => Result.fromMap(json.decode(str));
+
+    String toJson() => json.encode(toMap());
+
+    factory Result.fromMap(Map<String, dynamic> json) => Result(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        originalLanguage: originalLanguageValues.map[json["original_language"]],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: DateTime.parse(json["release_date"]),
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+    );
+
+    Map<String, dynamic> toMap() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+        "id": id,
+        "original_language": originalLanguageValues.reverse[originalLanguage],
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date": "${releaseDate.year.toString().padLeft(4, '0')}-${releaseDate.month.toString().padLeft(2, '0')}-${releaseDate.day.toString().padLeft(2, '0')}",
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+    };
+}
+
+enum OriginalLanguage { EN, JA }
+
+final originalLanguageValues = EnumValues({
+    "en": OriginalLanguage.EN,
+    "ja": OriginalLanguage.JA
+});
+
+class EnumValues<T> {
+    Map<String, T> map;
+    Map<T, String> reverseMap;
+
+    EnumValues(this.map);
+
+    Map<T, String> get reverse {
+        if (reverseMap == null) {
+            reverseMap = map.map((k, v) => new MapEntry(v, k));
+        }
+        return reverseMap;
+    }
+}
