1396b8f1b9a769e47b6967e5edce8b758e517d4d
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-02 01:05:15 -0700

    global theme + card swiper widget

---
+++ lib/widgets/card_swiper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 500,
+      color: Colors.indigo,
+    );
+  }
+}
