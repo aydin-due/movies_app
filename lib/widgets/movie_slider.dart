3d875e479abf6bba484d66ae4167598ab060bbcf
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-02 03:06:44 -0700

    movie poster widget

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -7,8 +7,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.indigo,
+      height: 260,
       child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
         const Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
@@ -16,6 +15,9 @@ class MovieSlider extends StatelessWidget {
               'Populares',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             )),
+
+        const SizedBox(height: 5,),
+
         Expanded( // soluciona error d tamaño, widget p/ abarcar todo el espacio disponible
           child: ListView.builder( // error en tamaño, xq widget padre es flexible
               scrollDirection: Axis.horizontal,
@@ -38,8 +40,29 @@ class _MoviePoster extends StatelessWidget {
     return Container(
       width: 130,
       height: 190,
-      color: Colors.green,
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-slider'),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'), 
+                image: NetworkImage('https://via.placeholder.com/300x400'),
+                width: 130,
+                height: 190,
+                fit: BoxFit.cover,),
+            ),
+          ) ,
+
+          const SizedBox(height: 5,),
+
+          const Text('Star Wars adjashdjsahdaskjdhas', 
+            overflow: TextOverflow.ellipsis, // muestra 3 puntitos p/ indicar q hay + texto
+            maxLines: 2, // num d lineas q hay
+            textAlign: TextAlign.center,)
+        ]),
     );
   }
 }
