a5bbbcbaebc82a65028cbfc9bc477c56ed124995
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-02 03:53:16 -0700

    poster & title widget

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -8,7 +8,11 @@ class DetailsScreen extends StatelessWidget {
             'no-movie'; //argumentos pasados en la nav
     return Scaffold(
         body: CustomScrollView(
-      slivers: [_CustomAppBar()],
+      slivers: [
+        //solo se pueden poner widgets hederados de fam sliver
+        _CustomAppBar(),
+        SliverList(delegate: SliverChildListDelegate([_PosterAndTitle()]))
+      ],
     ));
   }
 }
@@ -16,7 +20,7 @@ class DetailsScreen extends StatelessWidget {
 class _CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  SliverAppBar(
+    return SliverAppBar(
       backgroundColor: Colors.red,
       expandedHeight: 200,
       floating: false,
@@ -25,16 +29,80 @@ class _CustomAppBar extends StatelessWidget {
         centerTitle: true,
         titlePadding: const EdgeInsets.all(0),
         title: Container(
-          width: double.infinity,
-          alignment: Alignment.bottomCenter,
-          color: Colors.black12,
-          child: const Text('movie.title', 
-            style: TextStyle( fontSize: 16),)),
+            width: double.infinity,
+            alignment: Alignment.bottomCenter,
+            color: Colors.black12,
+            child: const Text(
+              'movie.title',
+              style: TextStyle(fontSize: 16),
+            )),
         background: const FadeInImage(
-          placeholder: AssetImage('assets/loading.gif'),
-          image: NetworkImage('https://via.placeholder.com/500x300'),
-          fit: BoxFit.cover),
+            placeholder: AssetImage('assets/loading.gif'),
+            image: NetworkImage('https://via.placeholder.com/500x300'),
+            fit: BoxFit.cover),
       ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: const FadeInImage(
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage('https://via.placeholder.com/200x300'),
+            height: 150,
+          ),
+        ),
+
+        const SizedBox( width: 20, ),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'movie.title',
+              style: textTheme.headline5,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            Text(
+              'movie.originalTitle',
+              style: textTheme.subtitle1,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+            ),
+            Row(
+              children: [
+                const Icon(
+                  Icons.start,
+                  size: 15,
+                  color: Colors.grey,
+                ),
+
+                const SizedBox(
+                  width: 5,
+                ),
+
+                Text(
+                  'movie.voteAverage',
+                  style: textTheme.caption,
+                )
+              ],
+            )
+          ],
+        )
+      ]),
     );
   }
 }
