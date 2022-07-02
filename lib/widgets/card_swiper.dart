ebd8a2fd7fcedfa6a4e904be30ed97d47031ab73
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-07-02 01:56:27 -0700

    card swiper w/ ClipRRect + card nav

--- lib/widgets/card_swiper.dart
+++ lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,29 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context)
+        .size; // widget q da info sobre el dispositivo en que corre
+
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.indigo,
+      height: size.height * 0.5,
+      child: Swiper(
+          itemCount: 10,
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.4,
+          itemBuilder: (_, int index) { // builder dispara una funcion dinamicamente
+            return GestureDetector(
+              onTap: () => Navigator.pushNamed(context, 'details', arguments: 'movie-instance'), //navega cuando se da click en card
+              child: ClipRRect( // card builder
+                borderRadius: BorderRadius.circular(20),
+                child: const FadeInImage(
+                    placeholder: AssetImage('assets/no-image.jpg'),
+                    image: NetworkImage('https://via.placeholder.com/300x400'),
+                    fit: BoxFit.cover,),
+              ),
+            );
+          }),
     );
   }
 }
